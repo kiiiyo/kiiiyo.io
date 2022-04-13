aaea63ec9940011c9758d1e06c09514be8d49974
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-04-14 04:51:33 +0900

    :sparkles: add teg-app page

---
+++ src/documents/teg-app.ts
@@ -0,0 +1,41 @@
+export const document = `
+# 💡 Overview
+
+このアプリケーションは東大式エゴグラム（Tokyo University Egogram）を実施できるアプリケーションです。今現在の自身の性格診断を実施することができます。
+
+### 注意事項
+
+設問の内容は公開できないためアプリケーションのUIのみ公開しています
+
+## Technologies
+
+### Language
+
+- [**TypeScript v4**](https://www.typescriptlang.org/)
+
+### UI Library
+
+- [**React v17**](https://reactjs.org/)
+- [**MUI v5**](https://mui.com/)
+- [**Chart.js**](https://www.chartjs.org/)
+
+### CSS Framework
+
+- [**Emotion**](https://emotion.sh/docs/introduction)
+
+### Development Tool
+
+- Build Tool
+  - [**Vite**](https://vitejs.dev/)
+- Lint & Formatter
+  - [**ESlint**](https://eslint.org/)
+  - [**Prettier**](https://prettier.io/docs/en/ignore.html)
+- Test Tool
+  - [**Jest**](https://jestjs.io/)
+  - [**React Testing Library**](https://testing-library.com/)
+- React Library Package
+  - [**React Router v6**](https://reactrouter.com/)
+  - [**Recoil**](https://recoiljs.org/)
+
+
+`
---
+++ src/pages/portfolios/teg-app/index.tsx
@@ -0,0 +1,63 @@
+import { FC, Fragment } from 'react'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+import ReactMarkdown from 'react-markdown'
+//
+import { Pages, Atoms } from '@/components'
+// TODO: MDXにする
+import { document } from '@/documents/teg-app'
+
+const Hero: FC = () => {
+  return (
+    <div className="py-8 bg-blue-500">
+      <div className="flex flex-row items-center p-8 mx-auto max-w-full sm:max-w-3xl md:py-16 lg:max-w-5xl">
+        <span className="block mr-4 text-4xl md:text-6xl">📋</span>
+        <h2 className="text-4xl font-bold text-white md:text-6xl">Teg App</h2>
+      </div>
+    </div>
+  )
+}
+
+const TegAppPage: NextPage = () => {
+  return (
+    <Pages.PortfolioSinglePage
+      hero={<Hero />}
+      sidebar={
+        <Fragment>
+          <Link href="https://teg-app-storybook.vercel.app/">
+            <a target="_blank">
+              <div className="flex items-center p-4 h-full bg-white hover:bg-neutral-50 active:bg-neutral-100 rounded-md shadow-sm">
+                <div className="mr-3">
+                  <Image width="32" height="32" alt="Storybook" src="/icon-storybook.svg" />
+                </div>
+                <div>
+                  <h2 className="font-bold text-neutral-800">Storybook</h2>
+                  <p className="text-sm text-neutral-400">Public</p>
+                </div>
+                <Atoms.Icon.ExternalLinkIcon className="ml-auto w-6 text-neutral-400" />
+              </div>
+            </a>
+          </Link>
+          <div className="mt-6">
+            <div className="flex items-center p-4 h-full bg-white hover:bg-neutral-50 active:bg-neutral-100 rounded-md shadow-sm">
+              <div className="mr-3">
+                <Image width="32" height="32" alt="Github" src="/icon-github.svg" />
+              </div>
+              <div>
+                <h2 className="font-bold text-neutral-800">Github</h2>
+                <p className="text-sm text-neutral-400">Private</p>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      }
+    >
+      <div className="markdown">
+        <ReactMarkdown>{document}</ReactMarkdown>
+      </div>
+    </Pages.PortfolioSinglePage>
+  )
+}
+
+export default TegAppPage
