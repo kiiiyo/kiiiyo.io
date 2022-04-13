9cf0f45533dc763f34e322b8ec042d2b2ce13f07
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-04-14 05:49:12 +0900

    :sparkles: add notfound page

--- src/documents/space-ui-kit.ts
+++ src/documents/space-ui-kit.ts
@@ -1,3 +1,35 @@
 export const document = `
-  ## aaa
+# 💡 Overview
+
+MUI ライブラリを利用したUI テンプレート アプリケーションです。
+
+## Technologies
+
+### Language
+  
+- [**TypeScript v4**](https://www.typescriptlang.org/)
+  
+### UI Library
+  
+- [**React v17**](https://reactjs.org/)
+- [**MUI v5**](https://mui.com/)
+  
+### CSS Framework
+  
+- [**Emotion**](https://emotion.sh/docs/introduction)
+  
+### Development Tool
+  
+- Build Tool
+  - [**Create React App**](https://create-react-app.dev/)
+- Lint & Formatter
+  - [**ESlint**](https://eslint.org/)
+  - [**Prettier**](https://prettier.io/docs/en/ignore.html)
+  - [**Stylelint**](https://stylelint.io/)
+- Test Tool
+  - [**Jest**](https://jestjs.io/)
+  - [**SWC**](https://swc.rs/)
+  - [**React Testing Library**](https://testing-library.com/)
+- React Library Package
+  - [**React Router v6**](https://reactrouter.com/)
 `
