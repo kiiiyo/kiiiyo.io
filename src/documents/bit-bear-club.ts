b75cca104ec819be7ce806f7a7163315f3b910b7
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-04-14 05:12:50 +0900

    :sparkles: add bbc page

---
+++ src/documents/bit-bear-club.ts
@@ -0,0 +1,35 @@
+export const document = `
+# 💡 Overview
+
+BitBearClubは🐻 クマをモチーフにしたピクセルアートをNFTの世界に届けるコレクターズプロジェクトです。
+
+## Technologies
+
+### Language
+
+- [**TypeScript v4**](https://www.typescriptlang.org/)
+
+### Framework
+
+- [**Next.js**](https://nextjs.org/)
+
+### UI Library
+
+- [**Tailwind CSS v3**](https://tailwindcss.com/)
+
+### CMS
+
+- [**Contentful**](https://www.contentful.com/)
+
+### Development Tool
+
+- Lint & Formatter
+  - [**ESlint**](https://eslint.org/)
+  - [**Prettier**](https://prettier.io/docs/en/ignore.html)
+- Test Tool
+  - [**Jest**](https://jestjs.io/)
+  - [**SWC**](https://swc.rs/)
+  - [**React Testing Library**](https://testing-library.com/)
+- React Library Package
+  - [**React Router v6**](https://reactrouter.com/)
+`
