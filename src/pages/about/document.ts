7000410aadcffd482d1309354ee8cf2eac30d607
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-04-13 21:22:04 +0900

    :lipstick: update style

--- src/pages/about/document.ts
+++ src/pages/about/document.ts
@@ -1,3 +1,23 @@
 export const document = `
-  ## Overview
-`
+# 基本情報
+
+- 氏名
+  - 清田 隆志
+- 生年月日
+  - 1985.05.21
+- 居住地
+  - 茨城県
+- 最終学歴
+  - 日本電子専門学校 コンピューターグラフィックス科
+
+## 各種アカウント
+
+- [Github](#)
+- [Twitter](#)
+- [Zenn](#)
+- [Linkdin](#)
+- [Wantedly](https://www.wantedly.com/id/takashi_kiyota_jp)
+
+
+
+  `
